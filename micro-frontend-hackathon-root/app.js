7c5a73a818416c34d31d37b10af812a6e26b1b07
majahar shaikh <mjhr.shaikh@gmail.com>
2021-02-04 15:48:08 +0530

    Add react web component in root app

--- micro-frontend-hackathon-root/app.js
+++ micro-frontend-hackathon-root/app.js
@@ -1,17 +1,16 @@
 const routes = {
-  '/'        :`<v-card title="Vue JS web component"></v-card>`,
-  '/angular' : `<header-app></header-app>`,
-  '/vue'     : `<v-card title="Vue JS web component"></v-card>`,
-  '/react'   : '<my-table id="myTable" title="test content"><div>Some Test</div></my-table>'
+  '/': `<v-card title="Vue JS web component"></v-card>`,
+  '/angular': `<header-app></header-app>`,
+  '/vue': `<v-card title="Vue JS web component"></v-card>`,
+  '/react': '<user-profile id="user-profile"></user-profile>'
 };
 
 const divs = {
-  '/'        : document.getElementById('root'),
-  '/angular' : document.getElementById('ang'),
-  '/vue'     : document.getElementById('vue'),
-  '/react'   : document.getElementById('react')
+  '/': document.getElementById('root'),
+  '/angular': document.getElementById('ang'),
+  '/vue': document.getElementById('vue'),
+  '/react': document.getElementById('react')
 };
-
 
 const rootDiv = document.getElementById('root');
 rootDiv.innerHTML = routes[window.location.pathname];
@@ -22,8 +21,12 @@ const onNavigate = (pathname) => {
     pathname,
     window.location.origin + pathname
   )
-   divs[pathname].innerHTML = routes[pathname]
+  divs[pathname].innerHTML = routes[pathname]
   //rootDiv.innerHTML = pathname
+  const userProfile = document.getElementById('user-profile');
+  userProfile.addEventListener('myEvent', () => {
+    $('a[href="#portfolio"]').click();
+  });
 }
 
 window.onpopstate = () => {
